7642719c11b1f8e81e57d1030b2626589df77137
skhrv <notebook1v@gmail.com>
2019-01-06 20:35:45 +0500

    add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,61 @@
 import { handleActions } from 'redux-actions';
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { omitBy } from 'lodash';
 import * as actions from '../actions';
 
 const messages = handleActions({
   [actions.newMessageSuccess]:
     (state, { payload: { data } }) => ({ ...state, [data.id]: data.attributes }),
+  [actions.removeChannelSuccess]:
+    (state, { payload: { data } }) => omitBy(state, value => value.channelId === data.id),
 }, {});
 
-const channels = (state = {}) => state;
+const channels = handleActions({
+  [actions.newChannelSuccess]:
+    (state, { payload: { data } }) => ({ ...state, [data.id]: data.attributes }),
+  [actions.editChannelSuccess]: (state, { payload: { data } }) => {
+    const { id, attributes } = data;
+    return { ...state, [id]: { ...state[id], ...attributes } };
+  },
+  [actions.removeChannelSuccess]:
+    (state, { payload: { data } }) => omitBy(state, value => value.id === data.id),
+}, {});
 
-const currentChannelId = handleActions({
+const channelUI = handleActions({
   [actions.chooseChannel]:
-    (state, { payload }) => payload,
-}, 1);
+    (state, { payload }) => ({ ...state, currentChannelId: payload }),
+  [actions.removeChannelSuccess]:
+    state => ({ ...state, currentChannelId: state.defaultChannelId }),
+}, {});
+
+const addChannelModal = handleActions({
+  [actions.closeAddChannelModal]:
+    () => ({ show: false }),
+  [actions.openAddChannelModal]:
+    () => ({ show: true }),
+}, {});
+
+const editChannelModal = handleActions({
+  [actions.closeEditChannelModal]:
+    () => ({ show: false }),
+  [actions.openEditChannelModal]:
+    (state, { payload }) => ({ show: true, editedChannel: payload }),
+}, {});
+
+const removeChannelModal = handleActions({
+  [actions.closeRemoveChannelModal]:
+    () => ({ show: false }),
+  [actions.openRemoveChannelModal]:
+    () => ({ show: true }),
+}, {});
+
 export default combineReducers({
   messages,
   channels,
-  currentChannelId,
+  channelUI,
+  addChannelModal,
+  editChannelModal,
+  removeChannelModal,
   form: formReducer,
 });
