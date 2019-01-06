aa067699e111aaba69ece18c668d33b0f8dd68f1
skhrv <notebook1v@gmail.com>
2019-01-06 20:36:06 +0500

    add Modals

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import ChannelsList from './ChannelsList';
 import MessageInput from './MessageInput';
 import Messages from './Messages';
+import AddChannelModal from './AddChannelModal';
+import EditChannelModal from './EditChannelModal';
+import RemoveChannelModal from './RemoveChannelModal';
 
 
 const App = () => (
   <>
     <div className="row" style={{ height: 'calc(100vh - 56px)' }}>
-      <div className="col-2">
+      <div className="col-4 col-md-3 col-lg-2 border-right">
         <ChannelsList />
       </div>
-      <div className="col d-flex flex-column h-100">
+      <div className="col-8 col-md-9 col-lg-10 d-flex flex-column h-100">
         <Messages />
         <MessageInput />
       </div>
     </div>
+    <AddChannelModal />
+    <EditChannelModal />
+    <RemoveChannelModal />
   </>
 );
 
