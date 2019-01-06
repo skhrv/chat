35d15b0e7745724dc6f849c96a216d58a1119206
skhrv <notebook1v@gmail.com>
2019-01-06 20:35:02 +0500

    add routes

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,6 @@ const host = '/api/v1';
 
 export default {
   messages: channelId => [host, `channels/${channelId}/messages`].join('/'),
+  channels: () => [host, 'channels'].join('/'),
+  channel: channelId => [host, `channels/${channelId}`].join('/'),
 };
