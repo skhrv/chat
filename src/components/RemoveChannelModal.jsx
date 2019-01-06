67f11bca3ed123218f152fcbe1d2449348982289
skhrv <notebook1v@gmail.com>
2019-01-06 20:37:41 +0500

    add RemoveChannelModal

---
+++ src/components/RemoveChannelModal.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { reduxForm, SubmissionError } from 'redux-form';
+import { Modal, Button } from 'react-bootstrap';
+
+import connect from '../connect';
+import { currentChannelSelector } from '../selectors';
+
+const mapStateToProps = state => ({
+  channels: state.channels,
+  currentChannelId: state.channelUI.currentChannelId,
+  defaultChannelId: state.channelUI.defaultChannelId,
+  show: state.removeChannelModal.show,
+  currentChannel: currentChannelSelector(state),
+});
+
+@connect(mapStateToProps)
+@reduxForm({ form: 'removeChannelModal' })
+class RemoveChannelModal extends React.Component {
+  handleClose = () => {
+    const { closeRemoveChannelModal } = this.props;
+    closeRemoveChannelModal();
+  }
+
+  handleSubmit = async () => {
+    const { closeRemoveChannelModal, removeChannel, currentChannelId } = this.props;
+    try {
+      await removeChannel(currentChannelId);
+    } catch (e) {
+      throw new SubmissionError({ _error: e.message });
+    }
+    closeRemoveChannelModal();
+  }
+
+  render() {
+    const {
+      show, submitting, handleSubmit, error, currentChannel,
+    } = this.props;
+
+    return (
+      <Modal show={show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add channel</Modal.Title>
+        </Modal.Header>
+        <form onSubmit={handleSubmit(this.handleSubmit)}>
+          <Modal.Body>
+            {`Are you sure you want to delete #${currentChannel.name}? All of the channel’s messages will be removed immediately. This cannot be undone.`}
+            {error && <span className="font-italic text-danger">{error}</span>}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit" disabled={submitting}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    );
+  }
+}
+export default RemoveChannelModal;
